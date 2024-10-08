742b1361237051f17a2b19a70dd3bd9ab5c6f412
Kaur <kaurterasmaa@gmail.com>
2024-10-08 21:52:17 +0300

    requirements as steam links

--- src/ModList.js
+++ src/ModList.js
@@ -6,13 +6,27 @@ const ModList = ({ mods, handleEdit, removeMod }) => {
             <h2>Installed Mods</h2>
             <ul>
                 {mods.map((mod, index) => (
-                    <li key={mod.workshopID}> {/* Use workshopID as key since it is unique */}
+                    <li key={mod.workshopID}>
                         <strong>{mod.modName || mod.name}</strong>
                         <p>Workshop ID: {mod.workshopID}</p>
                         <p>Mod ID: {mod.modID}</p>
                         <p>Map Folder: {mod.mapFolder}</p>
-                        <p>Requirements: {mod.requirements}</p>
-                        {/* Dynamically generate the Steam Workshop link based on the workshopID */}
+                        
+                        <p>Requirements: {
+                            mod.requirements?.split(';').map((reqID, i) => (
+                                <span key={i}>
+                                    <a 
+                                        href={`https://steamcommunity.com/sharedfiles/filedetails/?id=${reqID}`} 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                    >
+                                        {reqID}
+                                    </a>
+                                    {i < mod.requirements.split(';').length - 1 && '; '}
+                                </span>
+                            ))
+                        }</p>
+                        
                         <a 
                             href={`https://steamcommunity.com/sharedfiles/filedetails/?id=${mod.workshopID}`} 
                             target="_blank" 
@@ -22,9 +36,9 @@ const ModList = ({ mods, handleEdit, removeMod }) => {
                         </a>
                         <div>
                             <button onClick={() => handleEdit(index)}>Edit</button>
-                            <button onClick={() => removeMod(mod.workshopID)}>Remove</button> {/* Remove by workshopID */}
+                            <button onClick={() => removeMod(mod.workshopID)}>Remove</button>
                         </div>
-                        <p>Enabled: {mod.modEnabled ? 'Yes' : 'No'}</p> {/* Updated to use modEnabled */}
+                        <p>Enabled: {mod.modEnabled ? 'Yes' : 'No'}</p>
                     </li>
                 ))}
             </ul>
